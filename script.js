6e6c397f1ec879ffb60b3a7da4852305ba03f2ef
John Pourdanis <john.pourdanis@vimachem.com>
2024-04-19 10:46:07 +0300

    follow the workshop

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import http from "k6/http";
 import {sleep} from "k6";
+import { check } from "k6";
+import { Trend, Counter } from "k6/metrics";
 
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
 
@@ -13,6 +15,10 @@ export let options = {
     "http_req_duration": ["p(95)<5000"],
   },
 };
+
+// Put this after the options block, outside of the default function
+const pizzas = new Counter('quickpizza_number_of_pizzas');
+const ingredients = new Trend('quickpizza_ingredients');
 
 export default function () {
   let restrictions = {
@@ -30,5 +36,15 @@ export default function () {
     },
   });
   console.log(`${res.json().pizza.name} (${res.json().pizza.ingredients.length} ingredients)`);
+
+  // We increment the number of pizzas by 1
+pizzas.add(1);
+
+// We add the number of ingredients of the pizza to the trend
+ingredients.add(res.json().pizza.ingredients.length);
   sleep(1);
+
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
 }
